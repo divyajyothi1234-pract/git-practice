8ab861e3b1e708f32bdcc390925929506805e85d
Divya Jyothi Komatlapalli <divyajyothi1606@gmail.com>
2025-07-02 00:02:48 -0400

    added multiplication file

--- sample1.py
+++ sample1.py
@@ -2,5 +2,11 @@ def addition(a, b):
     """Returns the sum of a and b."""
     return a + b
 
-addition(5,3)
+
+
+def multiplication(a, b):
+    """Returns the product of a and b."""
+    return a * b
+
 print(addition(5, 3))
+print(multiplication(5, 3))
