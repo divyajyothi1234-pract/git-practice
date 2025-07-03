bc723d355561f53daa6da398f9ceac03b02149df
Divya Jyothi Komatlapalli <divyajyothi1606@gmail.com>
2025-07-02 20:28:49 -0400

    added subtraction function

--- sample1.py
+++ sample1.py
@@ -2,5 +2,17 @@ def addition(a, b):
     """Returns the sum of a and b."""
     return a + b
 
+def subtraction(a, b):
+    """Returns the difference of a and b."""
+    return a - b
+
+def multiplication(a, b):
+    """Returns the product of a and b."""
+    return a * b
+
+
+
 addition(5,3)
 print(addition(5, 3))
+print(subtraction(5, 3))
+print(multiplication(5, 3))
